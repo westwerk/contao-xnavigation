25772384cebc7c70ae53a3c862480cba0a41ada1
Tristan Lins <tristan.lins@bit3.de>
2014-06-30 10:08:17 +0200

    Add translations for the content element.

---
+++ contao/languages/en/default.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * xNavigation - Highly extendable and flexible navigation module for the Contao Open Source CMS
+ *
+ * Copyright (C) 2013 bit3 UG <http://bit3.de>
+ *
+ * @package    xNavigation
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @link       http://www.themeplus.de
+ * @license    http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Modules
+ */
+$GLOBALS['TL_LANG']['CTE']['navigation']       = 'Navigation';
+$GLOBALS['TL_LANG']['CTE']['xnavigation_menu'] = array(
+	'Extended navigation menu',
+	'Show an extended navigation menu.'
+);
